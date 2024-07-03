1379927febb40371a680880d5171edc6771d3ca6
simararustam <simararustam@gmail.com>
2024-07-03 15:20:14 +0400

    Integrat to  SQLAlchemy city model

---
+++ models/city.py
@@ -0,0 +1,39 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+from Persistence.data_manager import DataManager
+
+Base = declarative_base()
+
+class City(Base):
+    __tablename__ = 'cities'
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    country_id = Column(Integer, ForeignKey('countries.id'), nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    
+    def __init__(self, name, country_id, id=None, created_at=None, updated_at=None):
+        if id:
+            self.id = id
+        if created_at:
+            self.created_at = created_at
+        if updated_at:
+            self.updated_at = updated_at
+        
+        self.name = name
+        self.country_id = country_id
+    
+    @staticmethod
+    def get_all_cities(session):
+        return session.query(City).all()
+
+    @staticmethod
+    def get_city_by_id(session, city_id):
+        return session.query(City).filter(City.id == city_id).first()
+    
+    def country_id_check(self, country_id):
+        if isinstance(country_id, int):
+            return True
+        raise ValueError("Country ID must be numeric")
